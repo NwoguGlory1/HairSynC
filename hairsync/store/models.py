11070cfa4fb91afacefac9b713ae022de9465045
fae713 <favourmichael47@gmail.com>
2024-03-20 17:58:17 +0100

    Testing the category and products models

--- hairsync/store/models.py
+++ hairsync/store/models.py
@@ -4,11 +4,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.utils import timezone
+from django.utils.text import slugify
 
 class Category(models.Model):
     category_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True, editable=False)
+    slug = models.SlugField(max_length=255, unique=True, editable=False, default=slugify('default-category-slug'))
 
     def __str__(self):
         return self.name
@@ -19,21 +20,27 @@ class Category(models.Model):
             'name': self.name,
             'slug': self.slug,
         }
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
 
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255, null=False)
-    description = models.TextField(null=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity_in_stock = models.IntegerField(validators=[MinValueValidator(0)])
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False)
-    image = models.ImageField(upload_to='images', blank=True, null=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images', blank=True)
 
     def __str__(self):
         return f'{self.name} - {self.price}'
 
     def to_dict(self, request=None):
-        image_url = self.image.url
+        image_url = self.image.url if self.image else None
 
         if request and image_url:
             image_url = request.build_absolute_uri(image_url)
