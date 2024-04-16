804edd526a5f96d878a230c061a4df04471b7759
fae713 <favourmichael47@gmail.com>
2024-04-16 21:05:42 +0100

    added allowed host with 10000

--- hairsync/hairsync/settings.py
+++ hairsync/hairsync/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'DJANGO_SECRET_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost:10000',]
 
 
 # Application definition
